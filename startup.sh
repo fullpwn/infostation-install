414579e35cdcb08840c4c94d0664d427bc277377
miss australiana <git@missaustraliana.net>
2025-01-30 17:25:48 +1100

    ..

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ ping -c 1 8.8.8.8 > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Connected to the internet"
     cd /var/www/infostation-web/
-    git pull
+    sudo git pull
     startx
 else
     startx
