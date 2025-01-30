c16283511eb20a2fa9642390e83fa2ab7502d15d
miss australiana <git@missaustraliana.net>
2025-01-30 17:43:41 +1100

    .

--- startup.sh
+++ startup.sh
@@ -4,8 +4,9 @@ ping -c 1 8.8.8.8 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "Connected to the internet"
-    cd /var/www/infostation-web/
-    sudo git pull
+    cd /var/www/
+    sudo rm -rf infostation-web
+    sudo git clone https://github.com/fullpwn/infostation-web
     startx
 else
     startx
