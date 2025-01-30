b467fcd32af35f1b0af37efd1b13688866b5f08a
miss australiana <git@missaustraliana.net>
2025-01-30 17:03:09 +1100

    first commit

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# may as well attempt to fetch the latest interface, continue on if there isnt.
+ping -c 1 8.8.8.8 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Connected to the internet"
+    cd /var/www/html/
+    git clone https://github.com/fullpwn/infostation-web/ .
+    startx -nocursor
+else
+    startx -nocursor
+fi
